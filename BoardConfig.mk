0adb68d7a286d8f8ad6a50dbedcc2d2e1c785f93
sc4ryb3ar <sc4ryb3ar@gmail.com>
2017-09-26 15:47:48 -0400

    Changes match with kernel defconfig changes
    Change-Id: I4ce925ce4af2d1ede438eeefa444ef0595c0ef2f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,7 +131,7 @@ WCNSS_FILTER_USES_SIBS := true
 
 # Kernel
   BOARD_KERNEL_BASE := 0x80000000
-  BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff 
+  BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
   BOARD_KERNEL_PAGESIZE := 4096
   BOARD_KERNEL_TAGS_OFFSET := 0x00000100
   BOARD_RAMDISK_OFFSET     := 0x01000000
@@ -140,7 +140,7 @@ WCNSS_FILTER_USES_SIBS := true
   TARGET_KERNEL_ARCH := arm64
   TARGET_KERNEL_HEADER_ARCH := arm64
   TARGET_KERNEL_SOURCE := kernel/zte/msm8996
-  TARGET_KERNEL_CONFIG := lineageos_axon7_defconfig
+  TARGET_KERNEL_CONFIG := axon7nebula_defconfig
   TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # Keystore
@@ -222,7 +222,7 @@ TARGET_USES_INTERACTION_BOOST := true
   WIFI_DRIVER_FW_PATH_P2P := "p2p"
   WPA_SUPPLICANT_VERSION := VER_0_8_X
 
-  # EAS 
+  # EAS
   ENABLE_CPUSETS := true
   ENABLE_SCHEDBOOST := true
 
